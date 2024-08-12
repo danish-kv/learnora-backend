c456dc6438be6dfbf95ecadb7fde4c4313cca3a0
Danish <muhammeddanishkv05@gmail.com>
2024-08-12 22:58:55 +0530

    feat: added celery

---
+++ backend/celery.py
@@ -0,0 +1,18 @@
+from __future__ import absolute_import, unicode_literals
+import os
+from celery import Celery
+from django.conf import settings
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
+app = Celery('backend')  
+
+app.conf.enable_utc=False
+app.conf.update(timezone='Asia/Kolkata')
+
+app.config_from_object('django.conf:settings', namespace='CELERY')
+app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
+
+
+@app.task(bind=True)
+def debug_task(self):
+    print(f'Request: {self.request!r}')
